3bd6e997a36fafdcbb5c6a70dab21de01d92bad7
pascal <pascalcross15@gmail>
2018-03-10 12:02:06 +1300

    Basic soup logic

---
+++ Assets/Scripts/Soup.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Soup : MonoBehaviour
+{
+    public static Soup Instance = null;
+    public System.Action OnEmpty;
+
+    [SerializeField]
+    public float m_maxSoupLevel = 1000.0f;
+
+    [SerializeField]
+    public Image m_soupImage = null;
+
+    public float m_currentSoupLevel = 0.0f;
+
+    public float m_soupPercentage = 1.0f;
+
+    public void RemoveSoup(float amount)
+    {
+        m_currentSoupLevel -= amount;
+        UpdatePercentage();
+        UpdateSoupImage();
+
+        if (m_currentSoupLevel <= 0 && OnEmpty != null)
+        {
+            OnEmpty();
+            m_currentSoupLevel = 0;
+        }
+    }
+
+	// Use this for initialization
+	void Awake ()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            Initialise();
+        }
+        else
+        {
+            Destroy(this);
+        }
+	}
+
+    public void Initialise()
+    {
+        m_currentSoupLevel = m_maxSoupLevel;
+    }
+
+    private void UpdateSoupImage()
+    {
+        m_soupImage.transform.localScale = new Vector3(m_soupPercentage, m_soupPercentage, m_soupPercentage);
+    }
+
+    private void UpdatePercentage()
+    {
+        m_soupPercentage = m_currentSoupLevel / m_maxSoupLevel;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKeyUp(KeyCode.Space))
+        {
+            RemoveSoup(10);
+        }
+	}
+}
---
+++ Assets/Scripts/SoupTextPercentage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class SoupTextPercentage : MonoBehaviour
+{
+    private Text m_textPercentage = null;
+    private Soup m_soup = null;
+
+    private void Start()
+    {
+        m_textPercentage = GetComponent<Text>();
+        m_soup = Soup.Instance;
+    }
+
+    private void Update()
+    {
+        m_textPercentage.text = "SOUP LEFT: " + (m_soup.m_soupPercentage * 100) + "%";
+    }
+}
